3e21a1535702179c3d46288f350363a5a9b3ccba
hamza-kay <hamzahk1991@gmail.com>
2019-01-19 17:25:03 +0000

    Mobile first layout using Tailwind

---
+++ resources/views/mob-first.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Responsive Demo</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+
+
+
+</head>
+
+
+
+<body>
+
+
+  <div class="container mx-auto">
+
+      <div class="md:min-h-screen md:flex md:flex-col bg-grey">
+
+          <header class="bg-red p-3">
+              <h1>My Site</h1>
+          </header>
+
+
+          <div class="md:flex md:flex-1">
+
+              <aside class="bg-green">
+                  Sidebar
+              </aside>
+
+              <main class="bg-blue md:flex-1 p-3" >
+
+                  <div class="flex flex-wrap">
+
+                      <div class="md:w-1/4 p-3">
+
+                      <div class="bg-grey-light p-3 md:mb-2"> <p>Product Feature.</p> </div>
+
+                      </div>
+
+                      <div class="md:w-1/4 p-3">
+
+                          <div class="bg-grey-light p-3  md:mb-2"> <p>Product Feature.</p> </div>
+
+                      </div>
+
+                      <div class="md:w-1/4 p-3">
+
+                          <div class="bg-grey-light p-3  md:mb-2"> <p>Product Feature.</p> </div>
+
+                      </div>
+
+                      <div class="md:w-1/4 p-3">
+
+                          <div class="bg-grey-light p-3   md:mb-2"> <p>Product Feature.</p> </div>
+
+                      </div>
+
+                      <div class="md:w-1/4 p-3">
+
+                          <div class="bg-grey-light p-3  md:mb-2"> <p>Product Feature.</p> </div>
+
+                      </div>
+
+                      <div class="md:w-1/4 p-3">
+
+                          <div class="bg-grey-light p-3   md:mb-2"> <p>Product Feature.</p> </div>
+
+
+                      </div>
+
+                      <div class="md:w-1/4 p-3">
+
+                          <div class="bg-grey-light p-3   md:mb-2"> <p>Product Feature.</p> </div>
+
+                      </div>
+
+                      <div class="md:w-1/4 p-3">
+
+                          <div class="bg-grey-light p-3   md:mb-2"> <p>Product Feature.</p> </div>
+
+                      </div>
+
+
+
+                  </div>
+              </main>
+
+
+
+                  </div>
+
+
+
+          </div>
+
+          <footer class="bg-black text-white">
+              Copyright 2019
+          </footer>
+
+
+      </div>
+
+
+  </div>
+
+
+
+
+
+
+</body>
+
+
+</html>
